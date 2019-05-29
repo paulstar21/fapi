6d76be87b75a8c24717a8735109b6b84dea7278a
Ben Fourie <ben.fourie@gmail.com>
2019-05-29 18:05:10 +0200

    Add options to initialize the server with

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import express from "express";
 import router from "./router";
 import { swagger } from "./router";
 import defaults from "./router/defaults";
+import _ from "lodash";
 
 const server = templates => {
+  enableSwagger: true
+};
+
+export default function(templates, options) {
+  const ops = _.merge({}, defaultOptions, options);
   const app = express();
   app.use(defaults());
 
   if (templates) {
     app.use("/api", router(templates));
-    app.use("/docs", swagger(templates, "/api"));
+    if (ops.enableSwagger) {
+      app.use("/api-docs", swagger(templates, "/api"));
+    }
   }
   return app;
 };
