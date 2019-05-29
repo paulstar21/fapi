34daf0759444a3396cb2f5c6c9809e0f2a036123
Ben Fourie <ben.fourie@gmail.com>
2019-05-29 10:46:56 +0200

    Rename a test an use “default” middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 import router from "./router";
 import defaults from "./router/defaults";
 
 export default function(templates) {
   const app = express();
-  app.use(bodyParser.json());
+  app.use(defaults);
   if (templates) {
     app.use("/api", router(templates));
   }
